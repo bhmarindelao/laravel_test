7608c029f096a2d7048bfb546417d399de5f373d
bhmarindelao <brmarinag@gmail.com>
2023-01-09 14:50:28 -0600

    Migración tabla películas

--- laravel_test/database/migrations/2023_01_09_204621_create_movies_table.php
+++ laravel_test/database/migrations/2023_01_09_204621_create_movies_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+
+            $table->string('Photo');
+            $table->string('Name');
+            $table->year('Year');
+            $table->string('Genre');
+            $table->text('Description');
+            $table->string('Featuring');
+            $table->string('Director');
+
             $table->timestamps();
         });
     }
